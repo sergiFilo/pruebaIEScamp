a0f034a959e94428b86fd949727898d7abc4d600
sergio <sergio.gomez.alum@iescamp.es>
2020-05-20 11:54:19 +0200

    Creacion de la Clase 'Rectangulo' y del metodo 'obtenerArea'.

---
+++ src/q15_entornos_figuras/Q15_Entornos_Figuras.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q15_entornos_figuras;
+
+/**
+ *
+ * @author sergi
+ */
+public class Q15_Entornos_Figuras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
