3719008967d97fd6ec857018eb2c21c1526928a2
sergio <sergio.gomez.alum@iescamp.es>
2020-05-20 12:35:26 +0200

    Creacion nuevo metodo obtenerPerimetro

--- src/q15_entornos_figuras/Rectangulo.java
+++ src/q15_entornos_figuras/Rectangulo.java
@@ -11,4 +11,19 @@ package q15_entornos_figuras;
  */
 public class Rectangulo {
     
+    private double lado1;
+    private double lado2;
+
+    public Rectangulo(double lado1, double lado2) {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+    }
+    
+    public double obtenerArea(){
+        return lado1*lado2;
+    }
+    
+    public double obtenerPerimetro(){
+        return(lado1*2)+(lado2*2);
+    }
 }
